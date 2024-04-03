388a3a80fa61ab95356670c77da7d32bb1cea4b0
peihsuan1225 <jessica861225@gmail.com>
2024-04-04 03:00:34 +0800

    2024/4/4_3:00

--- script.js
+++ script.js
@@ -7,3 +7,10 @@ function popupMenu(){
 function closeMenu(){
     menu.style.display = "none";
 }
+
+window.addEventListener('resize', function() {
+    var screenWidth = window.innerWidth;
+    if (screenWidth > 600) {
+        menu.style.display=""; 
+    }
+});
