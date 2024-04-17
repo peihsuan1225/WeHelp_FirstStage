15cccf449a2ba9e639f9b888a1f69957422f7789
peihsuan1225 <jessica861225@gmail.com>
2024-04-17 13:35:24 +0800

    20240417_1335

---
+++ week3/task1.py
@@ -0,0 +1,69 @@
+import urllib.request as request
+import json
+url1="https://padax.github.io/taipei-day-trip-resources/taipei-attractions-assignment-1"
+url2="https://padax.github.io/taipei-day-trip-resources/taipei-attractions-assignment-2"
+# 存取url的內容，用json模組處理 json資料格式
+with request.urlopen(url1) as response1:
+    data1 = json.load(response1)
+
+results1 = data1["data"]["results"]
+
+with request.urlopen(url2) as response2:
+    data2 = json.load(response2)
+
+results2 = data2["data"]
+
+# 建立字典，存放 景點編號:景點隸屬行政區
+no_adress_dict = {}
+# 建立字典，存放 景點編號:捷運站
+no_mrt_dict = {}
+# 把地址分割處理，提取出區域(先用空格分割、再用"區"分割)
+for item in results2:
+    no_adress_dict[item["SERIAL_NO"]] = (item["address"].split(" ")[2]).split("區")[0]
+    no_mrt_dict[item["SERIAL_NO"]] = (item["MRT"])
+# print(no_adress_dict)
+
+
+# 開啟一個spot.csv檔案，寫入指定的內容
+with open("spot.csv","w",encoding="utf-8") as file:
+    # 遍歷 results1 取得對應的標題、編號、經緯度、圖片連結
+    for infos in results1:
+        SpotTitle = infos["stitle"] 
+        # 取得編號後，對應到字典中的隸屬行政區
+        spotNo = infos["SERIAL_NO"]
+        District = no_adress_dict[spotNo]
+        Longitude = infos["longitude"]
+        Latitude = infos["latitude"]
+        ImageURL = infos["filelist"]
+        FirstImageURL = ImageURL.split("https")[1] # 將 ImageURLs 以空格分割，並只取第一個圖片連結
+        file.write(SpotTitle + "," + District + "區" + "," + Longitude + "," + Latitude + "," + "https" + FirstImageURL + "\n")
+
+# 建立一個mtr_spot_dict字典，存放捷運站名(不重複):景點名
+mtr_spot_dict={}
+# 開啟一個mrt.csv檔案，寫入指定的內容
+with open("mrt.csv","w",encoding="utf-8") as file:
+    # 在第一個連結的資料中，取出景點編號對應到no_mrt_dict字典的捷運站，把捷運站加到mtr_spot_dict中當做key
+    for infos1 in results1:
+        spotNo = infos1["SERIAL_NO"]
+        MRT = no_mrt_dict[spotNo]
+        if MRT not in mtr_spot_dict:
+            mtr_spot_dict[MRT] = []
+    # 在第二個連結的資料中，取出景點編號、捷運站
+    for infos2 in results2:
+        spotNo = infos2["SERIAL_NO"]
+        MRT = infos2["MRT"]
+        # 將第二個連結取出的景點編號跟第一個連結取出的景點編號比對，如果相等在mtr_spot_dict把對應的捷運站key加上景點名稱的value
+        for infos1 in results1:
+            if spotNo == infos1["SERIAL_NO"]:
+                mtr_spot_dict[MRT].append(infos1["stitle"])
+    # print(mtr_spot_dict)
+    # 遍歷mtr_spot_dict的鍵值，印出捷運站,景點名稱1,景點名稱2,...
+    for mrt, spots in mtr_spot_dict.items():
+        file.write(mrt + ",")
+        for spot in spots:
+            file.write(spot + ",")
+        file.write("\n")
+
+   
+
+        
---
+++ week3/task2.py
@@ -0,0 +1,75 @@
+# 導入urllib.request模組 與 設定目的網址
+import urllib.request as req
+
+def getData(url,open_mode):
+    # 模擬真人透過瀏覽器造訪ptt樂透版的網頁請求
+    request = req.Request(url,headers={
+        "cookie":"over18=1",
+        "User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36"
+    })
+    # 透過請求造訪網頁，並獲取樂透版網頁內容存放在data變數中
+    with req.urlopen(request) as response:
+        data = response.read().decode("utf-8")
+    # 導入Beautiful Soup 4 模組
+    import bs4
+    # 打開一個article.csv的檔案
+    with open("article.csv",open_mode,encoding="utf-8") as file:
+        # 用bs4解析網頁的HTML內容
+        root = bs4.BeautifulSoup(data,"html.parser")
+        # 抓取 class=r-ent的div(裡面包含貼文的所有資訊)
+        rents = root.find_all("div",class_="r-ent")
+        for rent in rents:
+            # 抓取 在rent下 class=title 的 div
+            title = rent.find("div",class_="title")
+            # 如果文章標題沒有=None(即文章標題存在)
+            if title.a != None:
+                article_text = title.a.string
+                # 抓取 在rent下 class=hl 的 span
+                counts = rent.find("span",class_="hl")
+                # 如果沒有值，就帶入"0"
+                if counts is None:
+                    counts = "0"
+                for count in counts:
+                    counts_text = count
+                # 獲取文章連結
+                article_link = title.a["href"]
+                full_url = "https://www.ptt.cc"+ article_link
+                # 模擬真人透過瀏覽器造訪文章網頁的請求
+                request = req.Request(full_url,headers={
+                    "cookie":"over18=1",
+                    "User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36"
+                })
+                # 透過請求造訪網頁，並獲取文章網頁內容存放在article_data變數中
+                with req.urlopen(request) as response:
+                    article_data = response.read().decode("utf-8")
+                    # 用bs4解析文章網頁的HTML內容
+                    root_article = bs4.BeautifulSoup(article_data,"html.parser")
+                    
+                    # 抓取文章的貼文時間
+                    metalines = root_article.find_all("div", class_="article-metaline")
+                    for metaline in metalines:
+                        tag = metaline.find("span", class_="article-meta-tag")
+                        # print(metaline)
+                        if tag and tag.string == "時間":
+                            value = metaline.find("span", class_="article-meta-value")
+                            if value is None:
+                                date = ""
+                            else:
+                                date = value.string
+                # 寫入檔案(文章標題,按讚/倒讚數，發布時間)
+                file.write(f"{article_text},{counts_text},{date}\n")
+        # 回傳上一頁的連結
+        nextlink = root.find("a",string="‹ 上頁") #找到內文是‹ 上頁 的a標籤
+        return nextlink["href"] # 回傳這個標籤的href屬性
+
+pageurl="https://www.ptt.cc/bbs/Lottery/index.html"
+# 用迴圈來抓取前三頁的資料，並把檔案的mode設定為第一頁是w(清空>賦值)，二三頁是a(追加內容)
+count = 0
+while count<3:
+    if count == 0:
+        mode = "w"
+    else:
+        mode = "a"
+    pageurl="http://www.ptt.cc"+getData(pageurl,mode)
+    count +=1
+
