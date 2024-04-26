0073d0a26d6f2e8f222cc0e69b635467b385cf1a
peihsuan1225 <jessica861225@gmail.com>
2024-04-27 03:35:10 +0800

    20240427_0335

--- week4/main.py
+++ week4/main.py
@@ -1,7 +1,6 @@
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Request, Form
 from fastapi.templating import Jinja2Templates
-from fastapi.responses import JSONResponse, RedirectResponse
-from pydantic import BaseModel
+from fastapi.responses import RedirectResponse
 from starlette.middleware.sessions import SessionMiddleware
 
 app = FastAPI()
@@ -10,54 +9,48 @@ templates = Jinja2Templates(directory="templates")
 # 存取資料工具
 app.add_middleware(SessionMiddleware, secret_key="super-secret-key")
 
-# 根路徑，渲染 "index.html" 
+# 根路徑，套用 "index.html" 
 @app.get("/")
 def main_page(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
 
-# 定義帳號密碼的輸入值
-class SigninData(BaseModel):
-    username: str
-    password: str
-
-# signin路徑，判斷是否有缺少輸入的值，並顯示對應的錯誤訊息
-# 若輸入正確會回傳signed:Ture，錯誤會回傳signed:False並顯示對應的錯誤訊息
+# signin路徑，判斷是否有缺少輸入值，定義對應的錯誤訊息
+# 若輸入正確會導向member，紀錄SIGNED-IN為ture
+# 錯誤導向error並挾帶對應的錯誤訊息
 @app.post("/signin")
-async def signin(signin_data: SigninData, request: Request):
-    username = signin_data.username
-    password = signin_data.password
-    if not username or not password:
+async def signin(request: Request, username: str = Form(None), password: str = Form(None)):
+    if username == "test" and password == "test":
+        session = request.session
+        session["SIGNED-IN"] = True
+        return RedirectResponse(url="/member", status_code=302)
+    elif not username or not password:
         if not username and not password:
             error_message = "請輸入帳號、密碼"
         elif not username:
             error_message = "請輸入帳號"
         elif not password:
                 error_message = "請輸入密碼"
-        
-        return JSONResponse(status_code=400, content={"signed": False,  "message": error_message})
-
-    if username == "test" and password == "test":
-        request.session["user"] = {"username":"password"}
-        return{"signed":True}
+        return RedirectResponse(url=f"/error?message={error_message}", status_code=302)
     else:
-        return JSONResponse(status_code=400,content={"signed":False,"message":"帳號、或密碼輸入錯誤"})
+        return RedirectResponse(url="/error?message=帳號、或密碼輸入錯誤", status_code=302)
 
-# member路徑，會先確認是否已經有user數據(已登入)，如果有才渲染"member.html"，否則回到首頁
+# member路徑，會先確認SIGNED-IN是否為ture(已登入)，如果是才套用"member.html"，否則回到首頁
 @app.get("/member")
 async def member_page(request: Request):
-    user = request.session.get("user")
-    if user:
+    signed = request.session.get("SIGNED-IN")
+    if signed is True:
         return templates.TemplateResponse("member.html", {"request": request})
     else:
         return RedirectResponse(url="/")
 
-# error路徑，渲染 "error.html" 模板
+# error路徑，套用 "error.html" 
 @app.get("/error")
 async def error_page(request: Request):
     return templates.TemplateResponse("error.html", {"request": request})
 
-# signout路徑，移除用戶數據(已登出)，重新導向首頁
+# signout路徑，紀錄SIGNED-IN為false(已登出)，重新導向首頁
 @app.get("/signout")
 async def signout(request:Request):
-    request.session.pop("user", None)
-    return RedirectResponse(url="http://127.0.0.1:8000/")
+    session = request.session
+    session["SIGNED-IN"] = False
+    return RedirectResponse(url="/")
